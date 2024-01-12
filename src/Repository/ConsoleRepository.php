a1b6a4128561d64c7f5da3f9725c548c3d9c0925
arthrmchl <contact@arthurmichel.fr>
2024-01-12 13:36:41 +0100

    Create Console, Developer, Publisher and VideoGame entities

---
+++ src/Repository/ConsoleRepository.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Console;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Console>
+ *
+ * @method Console|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Console|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Console[]    findAll()
+ * @method Console[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class ConsoleRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Console::class);
+    }
+}
